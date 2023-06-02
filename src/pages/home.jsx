4430497b8368a768d0b6771477817792d36e0d64
ahnafi <devsulthon@gmail.com>
2023-06-02 14:10:54 +0700

    bug

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,12 +3,6 @@ import { Link } from "react-router-dom"
 
 
 function Homepage (){
-    const [login,setLogin] = useState(false)
-    useEffect(()=>{ 
-        if(!login){
-            window.location.replace("/login")
-        }
-    },[login])
     return (
         <div className="w-full justify-center items-center h-screen flex gap-3 text-xl">
         <h1>hello </h1>
